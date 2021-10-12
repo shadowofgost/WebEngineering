861bd56edd5c19f22600a053538765d9ffd4c8bf
shadowofgost <shadowofgost@outlook.com>
2021-10-12 19:27:51 +0800

    FastApi_V2.0

---
+++ .history/templates/Vue/src/App_20211012114055.vue
@@ -0,0 +1,28 @@
+<!--
+ * @Author: Albert Wang
+ * @LastEditors: Albert Wang
+ * @Date: 2021-10-12 09:02:42
+ * @LastEditTime: 2021-10-12 11:30:55
+ * @Description:basic 
+ * @FilePath: \\WebEngineering\\templates\\Vue\\src\\App.vue
+ * @Copyright Notice:  2021 Albert Wang 王子睿.All Rights Reserved.
+-->
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App"
+};
+</script>
+
+<style>
+#app {
+  padding: 0;
+  margin: 0;
+  overflow: hidden;
+}
+</style>
